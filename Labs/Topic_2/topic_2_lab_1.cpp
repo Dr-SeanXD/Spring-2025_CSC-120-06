7d7150dad022f08db2ac89fa5e38c01f41939144
Dr-SeanXD <150778070+Dr-SeanXD@users.noreply.github.com>
2025-02-17 23:55:02 -0500

    Add files via upload

---
+++ Labs/Topic_2/topic_2_lab_1.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author       : Sean Lin
+ * @Date Created : Feb. 12th, 2025
+ * @File Name    : topic_2_lab_1.cpp
+ * @Project      : Topic 2 Lab 1
+ * @Description  : Core commands for a C++ program, including the preprocessor commands to allow the user
+ *                 to use a string and to write or read from the console (character stream).
+ */
+
+//Preprocessor --> include the iostream file into this file.
+#include <iostream>
+
+//Specifying to use the std namespace.
+using namespace std;
+
+//Entry point (driver function) of the program.
+int main() {
+    //Printing "Hello World!".
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
